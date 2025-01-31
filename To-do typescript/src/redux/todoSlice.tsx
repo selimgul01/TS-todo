b274bacc770847b5385f478da3ae73d5f5a08f02
Selim GÜL <156907023+selimgul01@users.noreply.github.com>
2025-01-31 15:52:21 +0300

    Update todoSlice.tsx

--- To-do typescript/src/redux/todoSlice.tsx
+++ To-do typescript/src/redux/todoSlice.tsx
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { TodoInitialState, TodoProps, TodoType } from "../types/Types";
+import { TodoInitialState,  TodoType } from "../types/Types";
 
 const initialState:TodoInitialState = {
   todos:[]
